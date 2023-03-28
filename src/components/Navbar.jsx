82e24a2eab6d08fe9c86295fa8db8eac599a68a6
adenugbamichael <adenugbamicke@gmail.com>
2023-03-28 06:01:59 +0100

    fixed navbar transition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,17 +28,16 @@ const Navbar = () => {
           {navLinks.map((link) => (
             <li
               key={link.id}
-              className={`${
-                active === link.title
-                  ? "text-secondary after:scale-100 after:scale-x-100 after:scale-y-100  after:left-0 after:w-full "
-                  : "text-tertiary"
-              } `}
               onClick={() => {
                 setActive(link.title)
               }}
             >
               <a
-                className='text-[1rem] font-[300] cursor-pointer relative  after:bg-tertiary mobi'
+                className={`${
+                  active === link.title
+                    ? "text-secondary mobi--current"
+                    : "text-tertiary"
+                } mobi relative font-[300] text-[1rem]`}
                 href={`#${link.id}`}
               >
                 {link.title}
@@ -47,8 +46,6 @@ const Navbar = () => {
           ))}
         </ul>
         {/* Mobile Navigation Bar */}
-        {/* Mobile Navigation Bar */}
-        {/* Mobile Navigation Bar */}
         <div className=' flex flex-1 sm:hidden justify-end items-center'>
           <img
             src={toggle ? close : menu}
@@ -56,31 +53,30 @@ const Navbar = () => {
             className='z-10 object-contain cursor-pointer mx-2'
             onClick={() => setToggle(!toggle)}
           />
-
-          <div
-            className={`${
+          <ul
+            className={` ${
               !toggle ? "hidden" : "flex"
-            } max-h-[100vh] items-center duration-[0.2s] ease-out delay-[0s] transition-[max-h] m-0 p-0 list-none overflow-hidden flex flex-col justify-center bg-primary fixed top-0 bottom-0 left-0 right-0 z-2`}
+            } list-none flex animate myeffect flex-col justify-center  items-center max-h-[100vh] m-0 p-0 overflow-hidden bg-primary fixed top-0 bottom-0 left-0 right-0 z-2`}
           >
-            <ul className=' list-none flex  flex-col justify-center items-center  '>
-              {navLinks.map((link) => (
-                <li
-                  key={link.id}
+            {navLinks.map((link) => (
+              <li
+                className='pl-[15px]'
+                key={link.id}
+                onClick={() => setActive(link.title)}
+              >
+                <a
                   className={`${
-                    active === link.title ? "text-secondary" : "text-tertiary"
-                  } pl-[15px]`}
-                  onClick={() => setActive(link.title)}
+                    active === link.title
+                      ? "text-secondary mobi--current"
+                      : "text-tertiary"
+                  } text-[1rem]] cursor-pointer relative after:bg-tertiary fontsize ml-[2px] p-[1rem] inline-block mobi font-[300]`}
+                  href={`#${link.id}`}
                 >
-                  <a
-                    className='text-[1rem]] cursor-pointer relative after:bg-tertiary fontsize ml-[2px] p-[1rem] inline-block mobi '
-                    href={`#${link.id}`}
-                  >
-                    {link.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
